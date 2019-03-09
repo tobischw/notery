d7092b3eaaa65083f524539eeb4db673addd0b44
Tobias Schweiger <schweigert@objectcomputing.com>
2019-03-08 22:55:08 -0600

    login form post start

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -3,17 +3,50 @@ import React, { Component } from 'react';
 import logo from '../../resources/logo.png';
 
 import './index.css';
-import './LoginBox/';
+import {DefaultButton, Label, Link, PrimaryButton, TextField} from 'office-ui-fabric-react'
 
-import LoginBox from "./LoginBox"
+import config from '../../config';
 
 class Login extends Component {
-    state = {
-        redirectToReferrer: false
+    constructor() {
+        super();
+
+        this.state = {
+            username: '',
+            password: '',
+            redirectToReferrer: false
+        }
+
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+        this.onLoginClick = this.onLoginClick.bind(this);
     }
 
-    login = () => {
+    onLoginClick() {
+       // alert(this.ref.username.value);
+        console.log(this.state.username + ' ' + this.state.password);
 
+        fetch(config.server_url + '/api/login', {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username: this.state.username, password: this.state.password})
+        }).then(response => alert(response.json));
+    }
+
+    handleUsernameChange(value) {
+        this.setState({
+            username: value
+        })
+    }
+
+    handlePasswordChange(value) {
+        this.setState({
+            password: value
+        })
     }
 
     render() {
@@ -26,7 +59,16 @@ class Login extends Component {
 
         return <div className="login">
             <img className="logo" src={logo} alt="Logo" />
-            <LoginBox />
+            <div className="login-box">
+                <Label className="login-label">Login</Label>
+                <TextField onChanged={this.handleUsernameChange} required={true} placeholder="Username" />
+                <TextField onChanged={this.handlePasswordChange} type="password" required={true} placeholder="Password" />
+                <Link className="forgot-password" href="/">Forgot password?</Link>
+                <div className="button-box">
+                    <DefaultButton text="Create Account"/>
+                    <PrimaryButton text="Login" onClick={this.onLoginClick} />
+                </div>
+            </div>
         </div>
     }
 }
