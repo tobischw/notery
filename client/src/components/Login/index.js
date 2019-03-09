335d89551619892610310f83a4852872fbc890a7
Tobias Schweiger <schweigert@objectcomputing.com>
2019-03-09 00:18:21 -0600

    error message on failure

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import logo from '../../resources/logo-inverse.png';
 
 import './index.css';
-import {DefaultButton, Label, Link, PrimaryButton, TextField} from 'office-ui-fabric-react'
+import {DefaultButton, Label, Link, PrimaryButton, TextField, Dialog, DialogType, DialogFooter } from 'office-ui-fabric-react'
 
 import config from '../../config';
 
@@ -14,18 +14,17 @@ class Login extends Component {
         this.state = {
             username: '',
             password: '',
+            hideDialog: true
         }
 
         this.handleUsernameChange = this.handleUsernameChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.hideErrorDialog = this.hideErrorDialog.bind(this);
 
         this.submitLogin = this.submitLogin.bind(this);
     }
 
     submitLogin() {
-       // alert(this.ref.username.value);
-        console.log(this.state.username + ' ' + this.state.password);
-        console.log(JSON.stringify({username: this.state.username, password: this.state.password}));
         var data = {
             username: this.state.username,
             password: this.state.password
@@ -38,12 +37,17 @@ class Login extends Component {
             },
             body: JSON.stringify(data)
         }).then((res) => res.json())
-        .then((res) => {
-            // Handle Cookie Monster Here
-            // res contains the token as a json object
-            //console.log(res);
-        });
-        
+            .then((res) => {
+                // Handle Cookie Monster Here
+                // res contains the token as a json object
+                //console.log(res);
+            }).catch((e) => {
+                console.log(e);
+                this.setState({
+                    hideDialog: false
+                })
+            }
+        );
     }
 
     handleUsernameChange(value) {
@@ -58,19 +62,45 @@ class Login extends Component {
         })
     }
 
+    hideErrorDialog() {
+        this.setState({
+            hideDialog: true
+        })
+    }
+
     render() {
         return <div className="login">
-            <img className="logo" src={logo} alt="Logo" />
+            <img className="logo" src={logo} alt="Logo"/>
             <div className="login-box">
                 <Label className="login-label">Login</Label>
                 <TextField onChanged={this.handleUsernameChange} required={true} placeholder="Username" borderless/>
-                <TextField onChanged={this.handlePasswordChange} type="password" required={true} placeholder="Password"  borderless />
+                <TextField onChanged={this.handlePasswordChange} type="password" required={true} placeholder="Password"
+                           borderless/>
                 <Link className="forgot-password" href="/">Forgot password?</Link>
                 <div className="button-box">
                     <DefaultButton text="Create Account"/>
-                    <PrimaryButton text="Login" onClick={this.submitLogin} />
+                    <PrimaryButton text="Login" onClick={this.submitLogin}/>
                 </div>
             </div>
+            <Dialog
+                hidden={this.state.hideDialog}
+                onDismiss={this.hideErrorDialog}
+                dialogContentProps={{
+                    type: DialogType.normal,
+                    title: 'Invalid Login',
+                    subText: 'Make sure your username and password is correct.'
+                }}
+                modalProps={{
+                    titleAriaId: this._labelId,
+                    subtitleAriaId: this._subTextId,
+                    isBlocking: false,
+                    containerClassName: 'ms-dialogMainOverride'
+                }}
+            >
+                <DialogFooter>
+                    <DefaultButton onClick={this.hideErrorDialog} text="Close" />
+                </DialogFooter>
+            </Dialog>
         </div>
     }
 }
