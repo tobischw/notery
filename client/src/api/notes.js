31fcf78cba7b47c8603752604f7afac0cd57ffd9
Tobias Schweiger <schweigert@objectcomputing.com>
2019-03-09 21:29:38 -0600

    comments init

--- client/src/api/notes.js
+++ client/src/api/notes.js
@@ -1,23 +1,31 @@
 import socket from './socket.js';
 
 export function getNotesByGroup(groupID, callback) {
-    socket.emit('getNotesByGroup', { groupID: groupID }, (data) => {
+    socket.emit('getNotesByGroup', {groupID: groupID}, (data) => {
         callback(data);
     });
 }
+
 export function getNoteByID(noteID, callback) {
-    socket.emit('getNoteByID', { noteID: noteID }, (data) => {
+    socket.emit('getNoteByID', {noteID: noteID}, (data) => {
         callback(data);
     });
 }
+
+export function getComments(noteID, callback) {
+    socket.emit('getComments', {noteID: noteID}, (data) => {
+        callback(data);
+    });
+}
+
 export function createNote(name, groupID, callback) {
-    socket.emit('createNote', { name: name, groupID: groupID }, (data) => {
+    socket.emit('createNote', {name: name, groupID: groupID}, (data) => {
         callback(data);
     });
 }
 
 export function saveNote(noteID, value, callback) {
-    socket.emit('saveNote', { noteID: noteID, document: value }, (data) => {
+    socket.emit('saveNote', {noteID: noteID, document: value}, (data) => {
         callback(data);
     });
 }
